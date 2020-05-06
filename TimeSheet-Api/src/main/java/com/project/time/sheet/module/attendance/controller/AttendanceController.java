0de2e7ea1b4e864eaec548c3cb8f196f80fb47b5
natthapon-buakhiao <b5914890@gmail.com>
2020-05-06 17:12:44 +0700

    Edit AttendanceController

--- TimeSheet-Api/src/main/java/com/project/time/sheet/module/attendance/controller/AttendanceController.java
+++ TimeSheet-Api/src/main/java/com/project/time/sheet/module/attendance/controller/AttendanceController.java
@@ -1,26 +1,18 @@
 package com.project.time.sheet.module.attendance.controller;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.project.time.sheet.common.models.ResponseModel;
 import com.project.time.sheet.entity.Attendance;
 import com.project.time.sheet.module.attendance.models.request.ReqInsertAttendance;
-import com.project.time.sheet.module.attendance.models.request.reqAttendance;
 import com.project.time.sheet.module.attendance.service.AttendanceService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -54,27 +46,8 @@ public class AttendanceController {
 		return ResponseEntity.ok(res);
 	}
 
-	@RequestMapping(value = "/reqAttendance", method = RequestMethod.POST)
-   public String ReqAttendance(@RequestBody reqAttendance req) {
+	
 
-	RestTemplate restTemplate = new RestTemplate();
-      HttpHeaders headers = new HttpHeaders();
-      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-      HttpEntity<reqAttendance> entity = new HttpEntity<reqAttendance>(req,headers);
-
-      
-      return restTemplate.exchange(
-         "https://dev.priorsolution.co.th/iam/v2/auth/sign-in", HttpMethod.POST, entity, String.class).getBody();
-
-      // return "55555555555555555";
-   }
-
-   @RequestMapping(value = "/gg")
-   public String gg(){
-      
-      
-      return "ggggggggggggg";
-   }
 
 	
 }
