02432b422a2f72aee788c58321e3f7f847df4a75
natthapon-buakhiao <b5914890@gmail.com>
2020-05-06 17:21:14 +0700

    Edit API iam

--- TimeSheet-Api/src/main/java/com/project/time/sheet/module/iam/controller/IamController.java
+++ TimeSheet-Api/src/main/java/com/project/time/sheet/module/iam/controller/IamController.java
@@ -2,6 +2,7 @@ package com.project.time.sheet.module.iam.controller;
 
 import java.util.Arrays;
 
+import com.project.time.sheet.module.iam.models.ReqAuthentication;
 import com.project.time.sheet.module.iam.models.ReqProfile;
 
 import org.springframework.http.HttpEntity;
@@ -15,13 +16,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "/iam")
-public class UserController {
+public class IamController {
+    
+    @RequestMapping( value = "/login", produces = MediaType.APPLICATION_JSON_VALUE , method = RequestMethod.POST)
+    public String Authen(@RequestBody final ReqAuthentication req) {
+        final RestTemplate restTemplate = new RestTemplate();
+       final HttpHeaders headers = new HttpHeaders();
+       headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+       final HttpEntity<ReqAuthentication> entity = new HttpEntity<ReqAuthentication>(req,headers);
+       System.out.println(restTemplate.exchange(
+        "https://dev.priorsolution.co.th/iam/v2/auth/sign-in", HttpMethod.POST, entity, String.class).getBody());
+       return restTemplate.exchange(
+          "https://dev.priorsolution.co.th/iam/v2/auth/sign-in", HttpMethod.POST, entity, String.class).getBody();
+    }
 
-    @RequestMapping(value  = "/profile", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public String getProfile(@RequestBody final ReqProfile req){
+    @RequestMapping(value  = "/inquiryProfile", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public String inquiryProfile(@RequestBody final ReqProfile req){
         final RestTemplate restTemplate = new RestTemplate();
         final HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + req.getToken()
@@ -33,5 +46,5 @@ public class UserController {
         
 
     }
-    
+
 }
