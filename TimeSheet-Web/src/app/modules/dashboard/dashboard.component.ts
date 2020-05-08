08b2efde7c1695bdc9c126d397c5f572f5353b42
parinya77 <zenizawa79@gmail.com>
2020-05-08 16:17:23 +0700

    create Dashboard

--- TimeSheet-Web/src/app/modules/dashboard/dashboard.component.ts
+++ TimeSheet-Web/src/app/modules/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
+
+  back(){
+    this.router.navigate(['/login']);
+    sessionStorage.clear();
+  }
+
+  goAttendance(){
+    this.router.navigate(['/attendance']);
+  }
+
+  goSupervisor(){
+    this.router.navigate(['/supervisor']);
+  }
+
 
 }
