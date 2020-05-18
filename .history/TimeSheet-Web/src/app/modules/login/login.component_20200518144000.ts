6c66e7ac5f12e62b56875e5f67809d14e80d421a
natthapon-buakhiao <b5914890@gmail.com>
2020-05-18 14:42:54 +0700

    Edit service insertProfile

---
+++ .history/TimeSheet-Web/src/app/modules/login/login.component_20200518144000.ts
@@ -0,0 +1,129 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/service/login.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login, ReqProfile } from 'src/app/shared/model/reqLogin';
+import { RequestProfileService } from 'src/app/service/request-profile.service';
+import { ReqUserProfile } from 'src/app/shared/model/req-user-profile';
+import { UserProfileService } from 'src/app/service/user-profile.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  login: Login = new Login();
+  rec: Login;
+  formLogin: FormGroup;
+  submitted = false;
+  errorMessage: String;
+  createProfile: FormGroup;
+  setToken: any;
+  dataProfile: any;
+ 
+ 
+
+  constructor(
+    private _login: LoginService,
+    private fb: FormBuilder,
+    private router: Router,
+    private reqProfileService: RequestProfileService,
+    private userProfileService: UserProfileService,
+    private _FormBuild: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.createFormLogin();   
+  
+  }
+
+  get f() { return this.formLogin.controls; }
+
+  submit() {
+    this.login = this.formLogin.value;
+    this._login.login(this.login.userCode, this.login.password)
+    .subscribe(success => {
+      console.log(success);
+      if (success === true) {
+        this.router.navigate(['/dashboard']);
+        // this.router.navigateByUrl("/");
+        console.log('accessToken = ' + this._login.getTokens() );
+        this.getUserProfile();
+      }
+
+    });
+  }
+
+  createFormLogin(){
+    this.formLogin = this.fb.group({
+      userCode: ['', Validators.compose([
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.required
+      ])]
+    });
+  }
+
+  getUserProfile() {
+    let request = new ReqProfile();
+    this.setToken = JSON.parse(sessionStorage.getItem('accessToken'));
+    request.token = this.setToken;
+    console.log(request);
+
+    this.reqProfileService.getProfile(request).subscribe((res)  => {
+      this.dataProfile = res;
+      console.log( this.dataProfile);
+      this.setFromProfile(this.dataProfile);
+    }, (error) => {
+      console.log(error);
+     });
+    }
+
+    setFromProfile(dataProfile) {
+      this.createProfile = this._FormBuild.group({
+        userCode: [dataProfile.userCode,Validators.required],
+        firstName: [dataProfile.localFirstName,Validators.required],
+        lastName: [dataProfile.localLastName,Validators.required],
+        position: [dataProfile.position,Validators.required],
+        birthday: [null,Validators.required],
+        age: [null,Validators.required],
+        address: [null,Validators.required],
+        site: [dataProfile.department,Validators.required]
+      });
+      console.log( this.createProfile);
+      this.onSaveUserProfile();
+
+    }
+
+    onSaveUserProfile() {
+      let request = new ReqUserProfile();
+      request.userCode = this.createProfile.controls['userCode'].value;
+      request.firstName = this.createProfile.controls['firstName'].value;
+      request.lastName = this.createProfile.controls['lastName'].value;
+      request.birthday = this.createProfile.controls['birthday'].value;
+      request.age = this.createProfile.controls['age'].value;
+      request.address = this.createProfile.controls['address'].value;
+      request.position = this.createProfile.controls['position'].value;
+      request.site = this.createProfile.controls['site'].value;
+      this.userProfileService.insertProfile(request).subscribe((res) => {
+        console.log("edit UserProfile Success");
+        console.log(res);
+      },
+        (error) => {
+          console.log(error);
+        });
+    }
+
+  showAccessToken(){
+    var data = JSON.parse(sessionStorage.getItem('token')); // GET localStorage key = token มาดู
+    console.log("accessToken", data.accessToken)
+
+    // localStorage.removeItem('token'); เอาไว้ Remove Key 
+    // localStorage.clear เอาไว้ Remove clear localStorage
+    
+  }
+
+}
---
+++ .history/TimeSheet-Web/src/app/modules/user-profile/edit-user-profile-dialog/edit-user-profile-dialog.component_20200518144014.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { RequestProfileService } from 'src/app/service/request-profile.service';
+import { UserProfileService } from 'src/app/service/user-profile.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ReqProfile } from 'src/app/shared/model/reqLogin';
+import { Message } from 'src/app/shared/model/message';
+import { ReqUserProfile } from 'src/app/shared/model/req-user-profile';
+
+@Component({
+  selector: 'app-edit-user-profile-dialog',
+  templateUrl: './edit-user-profile-dialog.component.html',
+  styleUrls: ['./edit-user-profile-dialog.component.scss']
+})
+export class EditUserProfileDialogComponent implements OnInit {
+  @ViewChild("saveSwal", { static: false }) saveSwal: SwalComponent;
+  @ViewChild("saveSucessSwal", { static: false }) saveSucessSwal: SwalComponent;
+  editProfile: FormGroup;
+  submitted = false;
+  setToken: any;
+  dataProfile: any;
+
+
+
+  constructor(
+    public dialogRef: MatDialogRef<EditUserProfileDialogComponent>,
+    public dialog: MatDialog,    
+    private reqProfileService: RequestProfileService,
+    private _FormBuild: FormBuilder,
+    private userProfileService: UserProfileService,
+    private loading: NgxSpinnerService,
+  ) { }
+
+  ngOnInit() {
+    this.getUserProfile();
+    this.editProfile = new FormGroup({
+      userCode: new FormControl(),
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      position: new FormControl(),
+      birthday:new FormControl(),
+      age: new FormControl(),
+      address: new FormControl(),
+      site: new FormControl()
+    });
+  }
+
+  getUserProfile() {
+    let request = new ReqProfile();
+    this.setToken = JSON.parse(sessionStorage.getItem('accessToken'));
+    request.token = this.setToken;
+    console.log(request);
+
+    this.reqProfileService.getProfile(request).subscribe((res)  => {
+      this.dataProfile = res;
+      console.log( this.dataProfile);
+      this.setFromProfile(res);
+    }, (error) => {
+      console.log(error);
+     });
+    }
+
+    get f() { return this.editProfile.controls; }
+
+
+    setFromProfile(dataProfile) {
+      this.editProfile = this._FormBuild.group({
+        userCode: [dataProfile.userCode,Validators.required],
+        firstName: [dataProfile.localFirstName,Validators.required],
+        lastName: [dataProfile.localLastName,Validators.required],
+        position: [dataProfile.position,Validators.required],
+        birthday: [dataProfile.birthday,Validators.required],
+        age: [dataProfile.age,Validators.required],
+        address: [dataProfile.address,Validators.required],
+        site: [dataProfile.department,Validators.required]
+      });
+      console.log( this.editProfile);
+
+    }
+
+    onSubmit() {
+      this.submitted = true;
+      if (this.editProfile.invalid) {
+        return;
+      } else {
+        this.saveSwal.title = Message.MESSAGE_SAVE;
+        this.saveSwal.fire();
+      }
+    }
+
+    onSave() {
+      this.loading.show();
+      let request = new ReqUserProfile();
+      request.userCode = this.editProfile.controls['userCode'].value;
+      request.firstName = this.editProfile.controls['firstName'].value;
+      request.lastName = this.editProfile.controls['lastName'].value;
+      request.birthday = this.editProfile.controls['birthday'].value;
+      request.age = this.editProfile.controls['age'].value;
+      request.address = this.editProfile.controls['address'].value;
+      request.position = this.editProfile.controls['position'].value;
+      request.site = this.editProfile.controls['site'].value;
+      this.userProfileService.insertProfile(request).subscribe((res) => {
+        console.log("edit UserProfile Success");
+        console.log(res);
+        this.loading.hide();
+        this.saveSucessSwal.title = Message.MESSAGE_SAVE_SUCCESS;
+        this.saveSucessSwal.fire();     
+      },
+        (error) => {
+         this.loading.hide();
+          console.log(error);
+        });
+    }
+
+
+    canCle(status){
+      this.dialogRef.close(status)        
+    }
+
+
+}
