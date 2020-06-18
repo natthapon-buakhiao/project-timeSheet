3c24effb6d638175bc74e72be75fc7ed23e1d02e
parinya77 <zenizawa79@gmail.com>
2020-06-18 11:21:55 +0700

    ceate edit-attendance-dialog

---
+++ TimeSheet-Web/src/app/modules/attendance/edit-attendance-dialog/edit-attendance-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-attendance-dialog',
+  templateUrl: './edit-attendance-dialog.component.html',
+  styleUrls: ['./edit-attendance-dialog.component.scss']
+})
+export class EditAttendanceDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
